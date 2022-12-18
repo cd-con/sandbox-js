93ab333e0157f08ab67fe4a44bfcfb540334fab4
Terrarianec#7870 <48604002+Terrarianec@users.noreply.github.com>
2022-12-18 21:11:26 +0300

    Исправлена физика и добавлена рамка
    Добавлена рамка у игры;
    Рисование cumball'ов вынесено в отдельную функцию, так же слегка изменён принцип движения;
    Ball теперь является классом, некоторые свойства класса вынесены как константы;
    Появилась проблема с гравитацией (это уже второй проект, где гравитация всё ломает, потому что гравитация оказывается не той, которую хотелось бы);
    Добавлены новые функции, просто чтобы отличать, где движется, а где рисуется.

--- main.js
+++ main.js
@@ -1,69 +1,76 @@
 const canvas = document.getElementById("mainCanvas");
-
-let ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
 canvas.width = 512;
 canvas.height = 480;
 
-let cumballEntity = new Image();
+const cumballEntity = new Image();
 cumballEntity.src = 'content/cumball.png';
 
+const initialVelocity = {
+	x: 32,
+	y: 32
+};
+const friction = 0.8;
+const gravity = 4.88;
+const intervalBetweenFrames = 50; // In milliseconds
 
-function getCursorPosition(canvas, event) {
-    const rect = canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
-    console.log("x: " + x + " y: " + y);
-    return {x, y};
-}
+class Ball {
+	dx = initialVelocity.x;
+	dy = initialVelocity.y;
+	constructor(x, y, width = 16, height = 16) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	update() {
+		// Bounce off the edges
+		if (this.x + this.dx - this.width / 2 <= 0 || this.x + this.dx + this.width / 2 >= canvas.width)
+			this.dx = -this.dx * friction;
+
+		if (this.y + this.dy - this.height / 2 <= 0 || this.y + this.dy + this.height / 2 >= canvas.height)
+			this.dy = -this.dy * friction;
+		else
+			// Our only acceleration is gravity | Действительно, наше ускорение - только гравитация
+			this.dy += gravity; //Шары падают и прыгают, но сколько это будет продолжаться...
+
+		this.x = Math.min(Math.max(this.x + this.dx, this.width / 2), canvas.width - this.width / 2); //Побег не удастся
+		this.y = Math.min(Math.max(this.y + this.dy, this.height / 2), canvas.height - this.height / 2); //Вообще не удастся
+	};
+};
+
+let cumballInstances = [];
 
 canvas.addEventListener('mousedown', addXtraCumBall, false);
 
-let cumballInstances = [];
-function addXtraCumBall(mouseEvent){
-    const {x,y} = getCursorPosition(canvas, mouseEvent); 
-    cumballInstances.push(new Ball(x, y));
-}
-
-
-document.getElementById('play-again').addEventListener('click', event => {
-    cumballInstances = [];
+document.getElementById('play-again').addEventListener('click', () => {
+	cumballInstances = [];
 }, false);
 
-function Ball(x, y) {
-    this.x = x;
-    this.y = y;
-    this.dx = 32;
-    this.dy = 32;
-    this.friction = 0.8;
-    this.update = function() {
-      // Bounce off the edges
-      if (this.x + this.dx < 0 ||
-          this.x + this.dx > canvas.width) {
-        this.dx = -this.dx * this.friction;
-      }
-      if (this.y + this.dy < 0 ||
-          this.y + this.dy > canvas.height) {
-        this.dy = -this.dy * this.friction;
-      } else {
-        // Our only acceleration is gravity
-        this.dy += gravity;
-      }
-      
-      this.x = this.x + this.dx;
-      this.y = this.y + this.dy;
-      
-      ctx.drawImage(cumballEntity, this.x - this.dx / 2, this.y - this.dy / 2, 16,16);
-    };
-  }
-  let gravity = 4.88;
-  let timeStep = 50; // In milliseconds
-  let cmTID;
-  function updateAll() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    cumballInstances.forEach(entity => {entity.update()});
-    clearTimeout(cmTID);
-    cmTID = setTimeout(updateAll, timeStep);
-  }
+function getRelativeCursorPosition(canvas, event) {
+	const rect = canvas.getBoundingClientRect();
+	const x = event.clientX - rect.left;
+	const y = event.clientY - rect.top;
+	console.log("x: " + x + " y: " + y);
 
-  // Do the first update
-  updateAll();
+	return { x, y };
+}
+function addXtraCumBall(mouseEvent) {
+	const { x, y } = getRelativeCursorPosition(canvas, mouseEvent);
+	cumballInstances.push(new Ball(x, y));
+}
+
+function drawFrame() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	for (const cumball of cumballInstances) {
+		ctx.drawImage(cumballEntity, cumball.x - cumball.width / 2, cumball.y - cumball.height / 2, cumball.width, cumball.height);
+	}
+}
+function updateCumballs() {
+	for (const cumball of cumballInstances) {
+		cumball.update();
+	}
+}
+
+setInterval(drawFrame, intervalBetweenFrames);
+setInterval(updateCumballs, intervalBetweenFrames);
