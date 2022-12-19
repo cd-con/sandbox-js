5a838a8e56703235710c88f49e4798e6f2d5b470
Terrarianec#7870 <48604002+Terrarianec@users.noreply.github.com>
2022-12-19 18:38:49 +0300

    Переделаны следы и исправлены некоторые проблемы
    След больше не является классом (функции поинтереснее будут);
    След теперь отображается;
    Видимых изменений больше нет;
    Возможно, пришло время заменить кнопки-переключатели на что-то более нормальное и заменить в коде получение элемента по id на константы, в которых записано уже нужное значение (если кнопки так и останутся кнопками с функцией переключения состояния).

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const playAgainButton = document.getElementById('play-again')
 const canvas = document.getElementById("mainCanvas");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth - 32;
@@ -16,83 +17,50 @@ let deployEnabled = false; // deploy controller
 let trailEnabled = true;
 // BALL
 class Ball {
-	constructor(x, y, width = 16, height = 16) {
+	constructor(x, y, diameter = 16) {
 		this.x = x;
 		this.y = y;
-		this.width = width;
-		this.height = height;
+		this.diameter = diameter;
 		const angle = Math.random() * Math.PI * 2;
 		const acceleration = Math.random() * 16 + 16;
 		[this.dx, this.dy] = [Math.cos(angle) * acceleration, Math.sin(angle) * acceleration];
-		this.trail = new Trail();
 	}
 	update() {
 		// Bounce off the edges
-		if (this.x + this.dx - this.width / 2 <= 0 || this.x + this.dx + this.width / 2 >= canvas.width)
+		if (this.x + this.dx - this.diameter / 2 <= 0 || this.x + this.dx + this.diameter / 2 >= canvas.width)
 			this.dx = -this.dx * friction * 0.35;
 
-		if (this.y + this.dy - this.height / 2 <= 0 || this.y + this.dy + this.height / 2 >= canvas.height)
+		if (this.y + this.dy - this.diameter / 2 <= 0 || this.y + this.dy + this.diameter / 2 >= canvas.height)
 			this.dy = -this.dy * friction * 0.45;
 		else
 			// Our only acceleration is gravity | Действительно, наше ускорение - только гравитация
 			this.dy += Math.round(gravity * friction) / 10; //Шары падают и прыгают, но сколько это будет продолжаться...
 
-		this.x = Math.min(Math.max(this.x + this.dx, this.width / 2), canvas.width - this.width / 2); //Побег не удастся
-		this.y = Math.min(Math.max(this.y + this.dy, this.height / 2), canvas.height - this.height / 2); //Вообще не удастся
-		
-		if (trailEnabled){
-			this.trail.update(this.x, this.y);
-		}
+		this.x = Math.min(Math.max(this.x + this.dx, this.diameter / 2), canvas.width - this.diameter / 2); //Побег не удастся
+		this.y = Math.min(Math.max(this.y + this.dy, this.diameter / 2), canvas.height - this.diameter / 2); //Вообще не удастся
 	};
 };
-
-
-class Trail {
-	trailParticles = [];
-
-	constructor(length = 64, initSize = 16, rgbaArray = [255, 0, 0, 255]) {
-		this.length = length;
-		this.initSize = initSize;
-		this.colour = `rgba(${rgbaArray.join(", ")})`;
-	}
-
-	update(x, y) {
-		this.trailParticles.push({ x, y });
-
-		this.trailParticles.splice(this.trailParticles.length, Infinity);
-	}
-
-	draw(ctx) {
-		ctx.fillStyle = this.colour;
-		this.trailParticles.forEach((e, i) => {
-			const size = this.initSize - (this.initSize / length * i);
-			ctx.fillRect(e.x - size / 2, e.y - size / 2, size, size);
-		})
-
-	}
-}
 
 let cumballInstances = [];
 
 // Event listeners
 
-canvas.addEventListener('mousedown', (mouseEvent) => 
-{
+canvas.addEventListener('mousedown', (mouseEvent) => {
 	addXtraCumBall(getRelativeCursorPosition(canvas, mouseEvent));
 }, false);
 
-document.getElementById('play-again').addEventListener('click', () => {
+playAgainButton.addEventListener('click', () => {
 	cumballInstances = [];
 }, false);
 
 document.getElementById('auto-deploy').addEventListener('click', () => {
 	deployEnabled = !deployEnabled;
-	document.getElementById('auto-deploy').innerHTML = `Deploy balls automatically ${deployEnabled ? "✔": "❌"}`;
+	document.getElementById('auto-deploy').innerHTML = `Deploy balls automatically ${deployEnabled ? "✔" : "❌"}`;
 }, false);
 
 document.getElementById('draw-trail').addEventListener('click', () => {
 	trailEnabled = !trailEnabled;
-	document.getElementById('draw-trail').innerHTML = `Draw trails ${trailEnabled ? "✔": "❌"}`;
+	document.getElementById('draw-trail').innerHTML = `Draw trails ${trailEnabled ? "✔" : "❌"}`;
 }, false);
 
 function onPhysicsRefreshRateChange(interval) {
@@ -113,14 +81,39 @@ function addXtraCumBall(position) {
 
 // Interval functions
 
-function drawFrame() {
+function drawFrame(ctx) {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	for (const cumball of cumballInstances) {
-		ctx.drawImage(cumballEntity, cumball.x - cumball.width / 2, cumball.y - cumball.height / 2, cumball.width, cumball.height);
-		if (trailEnabled){
-			cumball.trail.draw(ctx);
+		if (trailEnabled) {
+			drawTrail(
+				{ x: cumball.x, y: cumball.y },
+				{ x: cumball.x - cumball.dx, y: cumball.y - cumball.dy },
+				cumball.diameter,
+				"rgba(127,127,127,255)", "rgba(0,0,0,0)"
+			);
 		}
+		ctx.drawImage(cumballEntity, cumball.x - cumball.diameter / 2, cumball.y - cumball.diameter / 2, cumball.diameter, cumball.diameter);
 	}
+}
+
+function drawTrail(start, end, initialSize, startColor, endColor) {
+	const angle = Math.atan2(end.y - start.y, end.x - start.x);
+	const upperPointAngle = angle - Math.PI / 2;
+	const lowerPointAngle = angle + Math.PI / 2;
+	const radius = initialSize / 2;
+
+	const linearGradient = ctx.createLinearGradient(start.x, start.y, end.x, end.y);
+	linearGradient.addColorStop(0, startColor);
+	linearGradient.addColorStop(1, endColor);
+
+	ctx.fillStyle = linearGradient;
+	ctx.beginPath();
+	ctx.moveTo(end.x, end.y);
+	ctx.lineTo(start.x + Math.cos(upperPointAngle) * radius, start.y + Math.sin(upperPointAngle) * radius);
+	ctx.lineTo(start.x + Math.cos(lowerPointAngle) * radius, start.y + Math.sin(lowerPointAngle) * radius);
+	ctx.closePath();
+	ctx.fill();
+
 }
 
 function updateCumballs() {
@@ -131,7 +124,7 @@ function updateCumballs() {
 
 function autoDeploy() {
 	if (deployEnabled) {
-		addXtraCumBall({x:canvas.width / 2, y:canvas.height / 2});
+		addXtraCumBall({ x: Math.random() * canvas.width, y: Math.random() * canvas.height });
 	}
 }
 
@@ -146,6 +139,6 @@ function getRelativeCursorPosition(canvas, event) {
 	return { x, y };
 }
 
-setInterval(drawFrame, intervalBetweenFrames);
+setInterval(drawFrame, intervalBetweenFrames, ctx);
 let updateInterval = setInterval(updateCumballs, intervalBetweenFrames);
 let autoDeployInterval = setInterval(autoDeploy, intervalBetweenBallDeploy);
