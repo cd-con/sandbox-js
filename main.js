e1f9e4a33d7c496e90b3c9fb871c687483fc4726
cd-con <61092992+cd-con@users.noreply.github.com>
2022-12-19 21:31:43 +0300

    Взрывы!
    Экспериментальная механика.
    Добавлен счётчик FPS.

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ canvas.height = window.innerHeight - 128;
 const cumballEntity = new Image();
 cumballEntity.src = 'content/cumball.png';
 
+const explosionSprites = new Image();
+explosionSprites.src = 'content/explosion.png';
+
 const friction = 0.9;
 const gravity = 9.81;
 
@@ -21,11 +24,14 @@ class Ball {
 		this.x = x;
 		this.y = y;
 		this.diameter = diameter;
+		this.state = "alive";
 		const angle = Math.random() * Math.PI * 2;
 		const acceleration = Math.random() * 16 + 16;
 		[this.dx, this.dy] = [Math.cos(angle) * acceleration, Math.sin(angle) * acceleration];
+		this.animationStage = 0;
 	}
-	update() {
+	async update() {
+		if (this.state == "alive"){
 		// Bounce off the edges
 		if (this.x + this.dx - this.diameter / 2 <= 0 || this.x + this.dx + this.diameter / 2 >= canvas.width)
 			this.dx = -this.dx * friction * 0.35;
@@ -38,10 +44,15 @@ class Ball {
 
 		this.x = Math.min(Math.max(this.x + this.dx, this.diameter / 2), canvas.width - this.diameter / 2); //Побег не удастся
 		this.y = Math.min(Math.max(this.y + this.dy, this.diameter / 2), canvas.height - this.diameter / 2); //Вообще не удастся
+		}
+		else{
+			explosionsArray.push({framerate:24,stage:1,maxStage:12,x:this.x,y:this.y,lastFrameTime:0});
+			ballInstances.splice(ballInstances.indexOf(this),1);
+		}
 	};
 };
 
-let cumballInstances = [];
+let ballInstances = [];
 
 // Event listeners
 
@@ -50,12 +61,16 @@ canvas.addEventListener('mousedown', (mouseEvent) => {
 }, false);
 
 playAgainButton.addEventListener('click', () => {
-	cumballInstances = [];
+	ballInstances = [];
 }, false);
 
 document.getElementById('auto-deploy').addEventListener('click', () => {
 	deployEnabled = !deployEnabled;
 	document.getElementById('auto-deploy').innerHTML = `Deploy balls automatically ${deployEnabled ? "✔" : "❌"}`;
+}, false);
+
+document.getElementById('detonate-balls').addEventListener('click', () => {
+	ballInstances.forEach((e) =>{e.state = "boom"})
 }, false);
 
 document.getElementById('draw-trail').addEventListener('click', () => {
@@ -75,15 +90,16 @@ function onAutodeployRateChange(interval) {
 }
 
 function addXtraCumBall(position) {
-	cumballInstances.push(new Ball(position.x, position.y));
-	document.getElementById("ball-counter").innerHTML = "Balls count: " + cumballInstances.length;
+	ballInstances.push(new Ball(position.x, position.y));
+	document.getElementById("ball-counter").innerHTML = "Balls count: " + ballInstances.length;
 }
 
 // Interval functions
 
 function drawFrame(ctx) {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	for (const cumball of cumballInstances) {
+	drawExplosions();
+	for (const cumball of ballInstances) {
 		if (trailEnabled) {
 			drawTrail(
 				{ x: cumball.x, y: cumball.y },
@@ -93,6 +109,26 @@ function drawFrame(ctx) {
 			);
 		}
 		ctx.drawImage(cumballEntity, cumball.x - cumball.diameter / 2, cumball.y - cumball.diameter / 2, cumball.diameter, cumball.diameter);
+	}
+	
+}
+
+let previosFrameTime, FPS;
+function calcFPS(){
+	FPS = Math.round(1000 / (performance.now() - previosFrameTime));
+	previosFrameTime = performance.now();
+}
+
+function drawFPS(){	
+	const uiElement = document.getElementById("fps-counter");
+	uiElement.innerHTML = `FPS(target): ${FPS} (${Math.round(1000 / intervalBetweenFrames)})`
+
+	if (FPS < Math.round(1000 / intervalBetweenFrames) - 12){
+		uiElement.style.color = "red";
+	}else if(FPS > Math.round(1000 / intervalBetweenFrames) + 4){
+		uiElement.style.color = "green";
+	}else{
+		uiElement.style.color = "blue";
 	}
 }
 
@@ -116,10 +152,28 @@ function drawTrail(start, end, initialSize, startColor, endColor) {
 
 }
 
+
+let explosionsArray = []
+function drawExplosions(){
+	explosionsArray.forEach((e)=>{
+		let time;
+		if(e.lastFrameTime + intervalBetweenFrames < performance.now() ){
+			time = performance.now();
+			e.lastFrameTime = time;
+			e.stage += 1;
+		}
+		if (e.stage > e.maxStage){
+			explosionsArray.splice(explosionsArray.indexOf(e),1);
+		}
+		const frameWidth =  explosionSprites.width - explosionSprites.width / e.stage;
+		ctx.drawImage(explosionSprites, frameWidth, 0, 96, 96, e.x - 48, e.y - 48, 96, 96);
+	})
+}
 function updateCumballs() {
-	for (const cumball of cumballInstances) {
+	for (const cumball of ballInstances) {
 		cumball.update();
 	}
+	calcFPS();
 }
 
 function autoDeploy() {
@@ -140,5 +194,6 @@ function getRelativeCursorPosition(canvas, event) {
 }
 
 setInterval(drawFrame, intervalBetweenFrames, ctx);
+setInterval(drawFPS, 50);
 let updateInterval = setInterval(updateCumballs, intervalBetweenFrames);
 let autoDeployInterval = setInterval(autoDeploy, intervalBetweenBallDeploy);
